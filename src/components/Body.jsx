eb5184f59c75a941550d1025e062234f4b2d8570
Shashi Raj <rishav.sinha477@gmail.com>
2025-08-22 17:13:26 +0530

    New cors

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -20,12 +20,16 @@ const Body = () => {
 
   const fetchapi = async () => {
     try {
-      const data = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-      );
+      const proxyUrl = "https://corsproxy.io/?";
+      const targetUrl =
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+      const data = await fetch(proxyUrl + encodeURIComponent(targetUrl));
       const json = await data.json();
+
       const restaurants =
         json?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
+
       setlistofResturant(restaurants);
       setfilterDetails(restaurants);
     } catch (err) {
